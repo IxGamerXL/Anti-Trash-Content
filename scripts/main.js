15571052ab888fdc30c5fbf295efcda50ed81abf
IxGamerXL but creative <90928500+IxGamerXL@users.noreply.github.com>
2021-11-04 11:04:23 -0500

    V2 - Javascript

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,26 @@
+//Block Remover
 Vars.content.blocks().each(e => {
 	var Trash = false; // Variable Reset
-
+	
 	// Function that makes finding trash easier
 	function wl(b){
 		if(e == b) Trash = true;
 	}
-
+	
 	wl(Blocks.conveyor);
 	wl(Blocks.titaniumConveyor);
 	wl(Blocks.router);
+	wl(Blocks.distributor);
 	wl(Blocks.duo);
 	wl(Blocks.differentialGenerator);
-
+	wl(Blocks.scorch);
+	wl(Blocks.conduit);
+	wl(Blocks.pulseConduit);
+	wl(Blocks.solarPanel);
+	wl(Blocks.solarPanelLarge);
+	wl(Blocks.pulverizer);
+	wl(Blocks.melter);
+	wl(Blocks.disassembler);
+	
 	if(Trash) e.buildVisibility = BuildVisibility.hidden; //Trash Disposer
-}) 
+})
